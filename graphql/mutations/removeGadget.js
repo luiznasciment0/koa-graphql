fca098c9fda4937bddbc27f60ec677fd9903c707
Luiz Nascimento <luizp.nascimento@outlook.com>
2021-02-25 01:01:44 -0300

    refactor

---
+++ graphql/mutations/removeGadget.js
@@ -0,0 +1,16 @@
+const { GraphQLString } = require('graphql')
+const gadgetGraphQLType = require('../gadgetType')
+const Gadget = require('../../models/gadgets')
+
+module.exports = {
+  type: gadgetGraphQLType,
+  args: {
+    id: { type: GraphQLString },
+  },
+  resolve(parents, args) {
+    return Gadget.findOneAndDelete(args.id).exec()
+      .then(gadget => gadget.remove())
+      .then(deletedGadget => deletedGadget)
+      .catch(err => console.log(err))
+  }
+}
